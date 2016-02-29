6bb304731d63333d6d39fd0ae96d86cff023dabf
AlkBur <alkboor@mail.ru>
2016-03-01 00:40:34 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,31 @@
-#include <iostream>
-#include "unistd.h"
-#include <string.h>
-#include <cstdlib>
-#include <cstdio>
-#include <fcntl.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <errno.h>
+#include <string>
+#include <pthread.h>
+#include <assert.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <sys/epoll.h>
-#include <sys/syslog.h>
-
-#include <netinet/tcp.h>
-#include <signal.h>
-
-#include <sys/stat.h>
 
 #include <sys/sendfile.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <cstring>
+#include <iostream>
+#include <signal.h>
 
-#define maxThreadsCount 4
-#define maxConnectionsCount 100
-
+//#define SERVERPORT 1111
+#define SERVERBACKLOG 10
+#define THREADSNO 20
 #define EVENTS_BUFF_SZ 256
+
+static int serversock;
+static int epoll_fd;
+static pthread_t threads[THREADSNO];
 
 struct globalArgs_t {
     std::string ip;         /* параметр -h */
@@ -29,14 +33,49 @@ struct globalArgs_t {
     int port;               /* параметр -p */
 };
 
-globalArgs_t globalArgs = {"127.0.0.1", "./", 8080};
+globalArgs_t globalArgs = {"127.0.0.1", "/home/alk/C++/WebServer", 8080};
 std::string s404 = "HTTP/1.0 404 Not Found\r\nContent-Type: text/html\r\n\r\n";
 
-static int ep_fd;
+
+
+void Sendrequest404(int &clientfd){
+    send(clientfd, s404.c_str(), s404.length(), 0);
+}
+
+int accept_new_client(void)
+{
+
+   int clientsock;
+   struct sockaddr_in addr;
+   socklen_t addrlen = sizeof(addr);
+   if ((clientsock = accept(serversock, (struct sockaddr *)&addr, &addrlen)) < 0) {
+       return -1;
+   }
+
+   char ip_buff[INET_ADDRSTRLEN + 1];
+   if (inet_ntop(AF_INET, &addr.sin_addr, ip_buff, sizeof(ip_buff)) == NULL) {
+       close(clientsock);
+       return -1;
+   }
+
+   //printf("*** [%p] Client connected from %s:%d\n", (void *)pthread_self(), ip_buff, ntohs(addr.sin_port));
+
+   struct epoll_event epevent;
+   epevent.events = EPOLLIN | EPOLLET;
+   epevent.data.fd = clientsock;
+
+   if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, clientsock, &epevent) < 0) {
+       perror("epoll_ctl(2) failed attempting to add new client");
+       close(clientsock);
+       return -1;
+   }
+
+   return 0;
+}
 
 int handle_request(int clientfd)
 {
-   char readbuff[512];
+    char readbuff[512];
    struct sockaddr_in addr;
    socklen_t addrlen = sizeof(addr);
    ssize_t n;
@@ -46,40 +85,43 @@ int handle_request(int clientfd)
    }
 
    if (n == 0) {
-       return -1;
+       return 0;
    }
 
    readbuff[n] = '\0';
+
+    //printf("readbuff %s\n", readbuff);
 
    char* save_ptr;
    char* command_line = strtok_r(readbuff, "\r\n", &save_ptr);
    if( command_line == NULL )
    {
-       //std::cout << "No comamnd line found in " << readbuff << "\n";
-       return -1;
+       Sendrequest404(clientfd);
+       return 0;
    }
+
    char* method = strtok_r(command_line, "\t ", &save_ptr);
    if( method == NULL )
    {
-       //std::cout << "No comamnd (GET) found in " << command_line << "\n";
-       return -1;
+       Sendrequest404(clientfd);
+       return 0;
    }
-   //std::cout << "method :" << method << ":\n";
    char* uri = strtok_r(NULL, "\t ", &save_ptr);
    if( uri == NULL )
    {
-       //std::cout << "No URI found in " << command_line << "\n";
-       return -1;
+       Sendrequest404(clientfd);
+       return 0;
    }
    //std::cout << "URI :" << uri << ":\n";
    char* file_path = strtok_r(uri, "?", &save_ptr);
    if( file_path == NULL )
    {
-       //std::cout << "No file path found in URI" << uri << "\n";
-       return -1;
+       Sendrequest404(clientfd);
+       return 0;
    }
 
    std::string sUsedFileName = file_path;
+
 
    if(sUsedFileName.empty()){
        //return -1;
@@ -91,163 +133,144 @@ int handle_request(int clientfd)
    sUsedFileName = globalArgs.directory + sUsedFileName;
 
    struct stat buf;
-   if(stat(sUsedFileName.c_str(), &buf)!=0){\
-       syslog(LOG_ERR, "stat");
-       return -1;
+   if(stat(sUsedFileName.c_str(), &buf)!=0){
+       //perror("stat");
+       //syslog(LOG_ERR, "stat");
+       Sendrequest404(clientfd);
+       return 0;
    }
     if (buf.st_mode & S_IFDIR){
-        syslog(LOG_ERR, "S_IFDIR");
-        return -1;
+        //syslog(LOG_ERR, "S_IFDIR");
+        Sendrequest404(clientfd);
+        return 0;
     }
 
 
-   syslog(LOG_ERR, sUsedFileName.c_str());
+   //syslog(LOG_ERR, sUsedFileName.c_str());
+    //printf("open\n");
 
     int NewHandle = open(sUsedFileName.c_str(), O_RDONLY);
     if(NewHandle==-1){
-        return -1;
+        Sendrequest404(clientfd);
+        return 0;
     }
     int NewSize = lseek(NewHandle, 0, SEEK_SET);
     if (NewSize == -1 || (NewSize = lseek(NewHandle, 0, SEEK_END)) == -1 || lseek(NewHandle, 0, SEEK_SET) == -1)
     {
         close(NewHandle);
+        Sendrequest404(clientfd);
+        return 0;
+    }
+
+    struct stat file_stat;
+    fstat(NewHandle, &file_stat);
+    const char replay_header_template[] =
+            "HTTP/1.0 200 OK\r\n"
+                    "Content-Type: text/html\r\n"
+                    "Content-Length: %lu\r\n\r\n";
+    char header_buf[4096];
+    int header_size = snprintf(header_buf, 4095, replay_header_template, file_stat.st_size);
+
+    ssize_t sent;
+    if ((sent = send(clientfd, header_buf, header_size, 0)) < 0) {
         return -1;
     }
+
     off_t Offset = 0;
     sendfile(clientfd, NewHandle, &Offset, NewSize-Offset);
 
-/*
-   int file_d = open(sUsedFileName.c_str(), O_RDONLY);
-   if( file_d >= 0 )
-   {
-       struct stat file_stat;
-       fstat(file_d, &file_stat);
-       const char replay_header_template[] =
-               "HTTP/1.0 200 OK\r\n"
-               "Content-Type: text/html\r\n"
-               "Content-Length: %lu\r\n\r\n";
-       char header_buf[4096];
-       int header_size = snprintf(header_buf, 4095, replay_header_template, file_stat.st_size);
+    //printf("sendfile2\n");
 
-       ssize_t sent;
-       if ((sent = send(clientfd, header_buf, n, 0)) < 0) {
-           return -1;
-       }
-       sendfile(clientfd, file_d, NULL, file_stat.st_size);
-   }
-   else
-   {
-       return -1;
-   }
-   */
    return 0;
 }
 
-void *server_thread(void *fd) {
-    //std::cout << "Create thread" << std::endl;
+void *worker_thr(void *args)
+{
+   struct epoll_event *events = (struct epoll_event *)malloc(sizeof(*events) * EVENTS_BUFF_SZ);
+   if (events == NULL) {
+       perror("malloc(3) failed when attempting to allocate events buffer");
+       pthread_exit(NULL);
+   }
 
-    int *sock = (int *)fd;
-    int localSock = *sock;
+   int events_cnt;
+   while ((events_cnt = epoll_wait(epoll_fd, events, EVENTS_BUFF_SZ, -1)) > 0) {
+       int i;
+       for (i = 0; i < events_cnt; i++) {
+           assert(events[i].events & EPOLLIN);
 
-    struct epoll_event ev, *events;
-    ep_fd = epoll_create(maxConnectionsCount);
-    events = (struct epoll_event *)malloc(sizeof(struct epoll_event) * EVENTS_BUFF_SZ);
-    if (events == NULL) {
-        //perror("malloc failed when attempting to allocate events buffer");
-        pthread_exit(NULL);
-    }
+           if (events[i].data.fd == serversock) {
+               if (accept_new_client() == -1) {
+                   fprintf(stderr, "Error accepting new client: %s\n", strerror(errno));
+               }
+           } else {
+               if (handle_request(events[i].data.fd) == -1) {
+                   fprintf(stderr, "Error handling request: %s\n", strerror(errno));
+               }
+               //printf("close\n");
+               close(events[i].data.fd);
+           }
+       }
+   }
 
-    ev.events = EPOLLIN | EPOLLERR | EPOLLHUP | EPOLLET;
-    ev.data.fd = localSock;
-    if (epoll_ctl(ep_fd, EPOLL_CTL_ADD,localSock, &ev) < 0) {
-        //perror("epoll_ctl failed");
-        pthread_exit(NULL);
-    }
+   if (events_cnt == 0) {
+       fprintf(stderr, "epoll_wait(2) returned 0, but timeout was not specified...?");
+   } else {
+       perror("epoll_wait(2) error");
+   }
 
-    while (1) {
-        //std::cout << "epoll_wait" << std::endl;
-        int num_fds = epoll_wait(ep_fd, events, EVENTS_BUFF_SZ, -1);
-        if (events->events & (EPOLLHUP | EPOLLERR)) {
-            //std::cerr << "epoll: EPOLLERR" << std::endl;
-            close(events->data.fd);
-            continue;
-        };
-        //std::cout << "data.fd" << events->data.fd << std::endl;
-        if (events->data.fd == localSock) {
+   free(events);
 
-            struct sockaddr remote_addr;
-            socklen_t addr_size = sizeof(remote_addr);
-
-            int connection = accept(localSock, &remote_addr, &addr_size);
-            if (connection == -1) {
-                //perror("connection");
-                continue;
-            };
-            fcntl(connection, F_SETFL,O_NONBLOCK | fcntl(connection, F_GETFL, 0));
-            ev.data.fd = connection;
-            epoll_ctl(ep_fd, EPOLL_CTL_ADD, connection, &ev);
-            continue;
-        }else{\
-            if (handle_request(events->data.fd) == -1) {
-                //fprintf(stderr, "Error handling request: %s\n", strerror(errno));
-                send(events->data.fd, s404.c_str(), s404.length(), 0);
-            }
-            close(events->data.fd);
-        }
-    };
+   return NULL;
 }
 
-void main_prog() {
-    openlog("WebServer", 0, LOG_USER);
-    syslog(LOG_ERR, "-----Statrt WebServer-------");
+int main_prog()
+{
+   if ((serversock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+       perror("socket(2) failed");
+       exit(EXIT_FAILURE);
+   }
 
-    /* Создание нового потокового сокета */
-    int sock = socket( AF_INET, SOCK_STREAM, IPPROTO_TCP );
+   struct sockaddr_in serveraddr;
+   serveraddr.sin_family = AF_INET;
+   serveraddr.sin_port = htons(globalArgs.port);
+   serveraddr.sin_addr.s_addr = INADDR_ANY;
 
-    int optval = 1;
-    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int)) < 0)
-        return;
-        //std::cerr << "Cannot set SO_REUSEADDR option "
-        //       << "on listen socket " << strerror(errno) << std::endl;
+   if (bind(serversock, (const struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0) {
+       perror("bind(2) failed");
+       exit(EXIT_FAILURE);
+   }
 
-    optval = 1;
-    if (setsockopt(sock, IPPROTO_TCP, TCP_NODELAY, &optval, sizeof(int)) < 0)
-        return;
-        //std::cerr << "Cannot set TCP_NODELAY option "
-        //       << "on listen socket " << strerror(errno) << std::endl;
+   if (listen(serversock, SERVERBACKLOG) < 0) {
+       perror("listen(2) failed");
+       exit(EXIT_FAILURE);
+   }
 
+   if ((epoll_fd = epoll_create(1)) < 0) {
+       perror("epoll_create(2) failed");
+       exit(EXIT_FAILURE);
+   }
 
-    struct sockaddr_in serveraddr;
-    serveraddr.sin_family = AF_INET;
-    serveraddr.sin_port = htons(globalArgs.port);
-    if(globalArgs.ip.empty() || globalArgs.ip == "127.0.0.1")
-        serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    else
-        serveraddr.sin_addr.s_addr = inet_addr(globalArgs.ip.c_str());
+   struct epoll_event epevent;
+   epevent.events = EPOLLIN | EPOLLET;
+   epevent.data.fd = serversock;
 
-    if (bind(sock, (const struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0) {
-        syslog(LOG_ERR, "Error bind");
-    //std::cerr << "bind failed " << strerror(errno) << std::endl;
-        exit(EXIT_FAILURE);
-    }
+   if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, serversock, &epevent) < 0) {
+       perror("epoll_ctl(2) failed on main server socket");
+       exit(EXIT_FAILURE);
+   }
 
-    if (listen(sock, 64) < 0) {
-        syslog(LOG_ERR, "Error listen");
-        //std::cerr << "listen " << strerror(errno) << std::endl;
-        exit(EXIT_FAILURE);
-    }
+   int i;
+   for (i = 0; i < THREADSNO; i++) {
+       if (pthread_create(&threads[i], NULL, worker_thr, NULL) < 0) {
+           perror("pthread_create(3) failed");
+           exit(EXIT_FAILURE);
+       }
+   }
 
+   /* main thread also contributes as worker thread */
+   worker_thr(NULL);
 
-    pthread_t th[maxThreadsCount];
-    for(int t_cnt = 0; t_cnt < maxThreadsCount; t_cnt++ ) {
-        int status = pthread_create(&th[t_cnt], nullptr, (void*(*)(void *)) server_thread,(void *)&sock);
-        if( status != 0 ) {
-            //perror( "pthread_create" );
-        syslog(LOG_ERR, "pthread_create");
-            exit(EXIT_FAILURE);
-        }
-    };
-    pause();
+   return 0;
 }
 
 void daemonize() {
@@ -266,8 +289,8 @@ void daemonize() {
 
             main_prog();
         case -1:
-        std::cout << "Error fork";
-        break;
+            std::cout << "Error fork";
+            break;
         default:
             break;
     }
@@ -295,20 +318,20 @@ int main(int argc, char* argv[])
     //	opterr=0; //если нужно запретить ошибки
     while ( (opt = getopt(argc,argv,optString)) != -1) {
         switch (opt) {
-        case 'h':
-            globalArgs.ip = optarg;
-            break;
-        case 'p':
-            globalArgs.port = atoi(optarg);
-            break;
-        case 'd':
-            globalArgs.directory = optarg;
-            break;
-        case '?':
-            //std::cerr << "Error found !" << optarg << std::endl;
-            break;
-        default:
-            break;
+            case 'h':
+                globalArgs.ip = optarg;
+                break;
+            case 'p':
+                globalArgs.port = atoi(optarg);
+                break;
+            case 'd':
+                globalArgs.directory = optarg;
+                break;
+            case '?':
+                //std::cerr << "Error found !" << optarg << std::endl;
+                break;
+            default:
+                break;
         }
     }
 
